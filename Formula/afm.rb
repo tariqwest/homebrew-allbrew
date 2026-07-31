d314d55e7ee843c0a354d937657f42ae3900712c
Tariq West <tariqwest@gmail.com>
2026-07-31 02:13:13 -0400

    chore(allbrew): add afm

--- Formula/afm.rb
+++ Formula/afm.rb
@@ -2,22 +2,29 @@ class Afm < Formula
   desc "Command-line tool for Apple's Foundation Models framework."
   homepage "https://github.com/rudrankriyam/Foundation-Models-Framework-CLI"
   license "MIT"
-  url "https://api.github.com/repos/rudrankriyam/Foundation-Models-Framework-CLI/tarball/0.1.0"
-  sha256 "ecd3c98040807965cc7b724211e021ee759ace7091bb8c611eef619a87388254"
-  head "https://github.com/rudrankriyam/Foundation-Models-Framework-CLI.git", branch: "main"
+  version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/rudrankriyam/Foundation-Models-Framework-CLI/releases/download/#{version}/afm_#{version}_macOS_universal"
+      sha256 "476555de227aab44042874b4b928f6f0907ef65805de4ebef74d1f0fa0e0953e"
+    end
+    on_intel do
+      url "https://github.com/rudrankriyam/Foundation-Models-Framework-CLI/releases/download/#{version}/afm_#{version}_macOS_universal"
+      sha256 "476555de227aab44042874b4b928f6f0907ef65805de4ebef74d1f0fa0e0953e"
+    end
+  end
 
   livecheck do
-    url :head
+    url :stable
     strategy :github_latest
   end
 
   def install
-    system "make", "PREFIX=#{prefix}", "install"
-  end
-
-  service do
-    run [opt_bin/"afm", "serve"]
-    keep_alive true
+    bin_path = Dir["*"].find { |f| File.file?(f) && File.executable?(f) }
+    bin_path ||= Dir["*"].find { |f| File.file?(f) && !f.end_with?(".txt", ".sha256", ".sig", ".asc") }
+    odie "No binary found in download" unless bin_path
+    bin.install bin_path => "afm"
   end
 
   test do
